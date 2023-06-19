c16b9ffbc7db8cbd4e1edabf71bfb2a6da4930cb
Maksym <maksym.gromov82@gmail.com>
2023-06-19 16:43:58 +0300

    Задание 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+let intervalId = null;
+
+function onStartBtnClick() {
+  intervalId = setInterval(setRandomColor, 1000);
+  stopBtn.removeAttribute('disabled');
+  startBtn.setAttribute('disabled', 'true');
+}
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  stopBtn.setAttribute('disabled', 'true');
+  startBtn.removeAttribute('disabled');
+}
+function setRandomColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
